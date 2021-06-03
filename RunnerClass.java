dcdf6b3c77b83fdd26402bf196ae3a0f2a61194e
Muthu-Anand <84925057+Muthu-Anand@users.noreply.github.com>
2021-06-03 08:59:03 +0530

    Add files via upload

---
+++ RunnerClass.java
@@ -0,0 +1,14 @@
+package com.runner.org;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "additoin\\java.core\\AdaptinProject\\featureFile")
+
+
+public class RunnerClass {
+
+}
